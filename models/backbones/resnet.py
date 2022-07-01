50dbae917e3e94f49eee4c8afb09fd8f5f60e7f4
liaochiheng <liaochiheng@gmail.com>
2022-07-01 12:15:44 +0800

    只留unet

--- models/backbones/resnet.py
+++ models/backbones/resnet.py
@@ -10,7 +10,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 from collections import OrderedDict
 
-
 #------------------------------------------------------------------------------
 #   ResNetBlock
 #------------------------------------------------------------------------------
